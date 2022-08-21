24d3cf167572d5352f03696ba4f2d7a994998838
Dim@ <dzmitry.kaporski@gmail.com>
2022-08-21 13:33:48 +0300

    refactor: change structure of the project

--- src/app/aside/aside.data.ts
+++ src/app/aside/aside.data.ts
@@ -7,20 +7,38 @@ export const CONTACTS: Contacts[] = [
     iconTelegram: "/assets/svg/telegram.svg",
     iconViber: "/assets/svg/viber.svg"
   },
-  { icon: "/assets/svg/mail.svg", contact: "dzmitry.kaporski@gmail.com" },
-  { icon: "/assets/svg/linkedin.svg", link: "https://www.linkedin.com/in/kaporski/", contact: "linkedin.com" },
-  { icon: "/assets/svg/skype.svg", contact: "live:.cid.d6899aaa40ee068d" },
-  { icon: "/assets/svg/github.svg", link: "https://github.com/DzmitryKaporski", contact: "github.com" },
+  {
+    icon: "/assets/svg/mail.svg",
+    contact: "dzmitry.kaporski@gmail.com"
+  },
+  {
+    icon: "/assets/svg/linkedin.svg",
+    link: "https://www.linkedin.com/in/kaporski/",
+    contact: "linkedin.com"
+  },
+  {
+    icon: "/assets/svg/skype.svg",
+    contact: "live:.cid.d6899aaa40ee068d"
+  },
+  {
+    icon: "/assets/svg/github.svg",
+    link: "https://github.com/DzmitryKaporski",
+    contact: "github.com"
+  },
 ]
 
 export const SKILLS: Skills[] = [
+  { name: "HTML5", value: 7 },
   { name: "CSS3", value: 7 },
   { name: "JS", value: 7 },
+  { name: "React", value: 4 },
+  { name: "Angular", value: 5 },
+  { name: "Firebase", value: 5 },
   { name: "Node.js", value: 6 },
+  { name: "Reveal.js", value: 7 },
   { name: "TypeScript", value: 6 },
   { name: "Webpack", value: 3 },
+  { name: "Figma", value: 7 },
   { name: "Git", value: 7 },
-  { name: "React", value: 2 },
-  { name: "Angular", value: 5 },
   { name: "English: pre-intermediate", value: 4 },
 ]
--- src/app/main/main.data.ts
+++ src/app/main/main.data.ts
@@ -14,8 +14,8 @@ export const EDUCATION: Education[] = [
 ]
 
 export const LINKS: Links[] = [
-  { title: "Website", href: "https://dzmitrykaporski.github.io/website-tamplate/", src: "assets/img/1.avif" },
-  { title: "Angular CRUD", href: "https://dk-angular-crud.surge.sh/", src: "assets/img/2.avif" },
+  { title: "Template website", href: "https://dzmitrykaporski.github.io/website-tamplate/", src: "assets/img/1.avif" },
   { title: "English for children", href: "https://dzmitrykaporski-jsfe2021q1-english-for-kids.surge.sh/", src: "assets/img/3.avif" },
-  { title: "Angular application", href: "https://final-project_angular13.surge.sh/", src: "assets/img/4.avif" },
+  { title: "Angular movie app", href: "https://final-project_angular13.surge.sh/", src: "assets/img/4.avif" },
+  { title: "Angular CRUD", href: "https://dk-angular-crud.surge.sh/", src: "assets/img/2.avif" },
 ]
