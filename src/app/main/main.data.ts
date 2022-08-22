7d4e80ff5d23994af6de6e0164b8b707102bb67f
Dim@ <dzmitry.kaporski@gmail.com>
2022-08-22 19:11:12 +0300

    refactor: change structure of the project

--- src/app/main/main.data.ts
+++ src/app/main/main.data.ts
@@ -1,16 +1,16 @@
 import { Courses, Education, Links } from './main.interface';
 
 export const COURSES: Courses[] = [
-  { year: "2020", link: "assets/documents/academy.avif", name: "STEP IT Academy (Frontend development / React)" },
-  { year: "2021", link: "assets/documents/english.avif", name: "STREAMLINE English (Pre-intermediate)" },
-  { link: "assets/documents/rsschool.avif", name: "THE ROLLING SCOPES SHCOOL (Frontend development / JavaScript)" },
-  { link: "assets/documents/epam.avif", name: "EPAM (Frontend development / Angular)" },
+  { year: "2020", link: "assets/documents/academy.jpg", name: "STEP IT Academy (Frontend development / React)" },
+  { year: "2021", link: "assets/documents/english.jpg", name: "STREAMLINE English (Pre-intermediate)" },
+  { link: "assets/documents/rsschool.jpg", name: "THE ROLLING SCOPES SHCOOL (Frontend development / JavaScript)" },
+  { link: "assets/documents/epam.jpg", name: "EPAM (Frontend development / Angular)" },
   { year: "2022", name: "EPAM Learn digital platform (Version Control with Git)" },
 ]
 
 export const EDUCATION: Education[] = [
   { year: "1999 - 2002", name: "Mogilev State Polytechnic College" },
-  { year: "2007 - 2011", link: "assets/documents/mesi.avif", name: "Moscow State University of Economics, Statistics and Informatics" },
+  { year: "2007 - 2011", link: "assets/documents/mesi.jpg", name: "Moscow State University of Economics, Statistics and Informatics" },
 ]
 
 export const LINKS: Links[] = [
