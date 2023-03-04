5f3244c4714820f2e91adee122438a7a3b10c9f6
dima <dzmitry.kaporski@gmail.com>
2023-03-04 20:46:24 +0300

    fix: implement correct picture format

--- src/app/main/main.data.ts
+++ src/app/main/main.data.ts
@@ -14,8 +14,8 @@ export const EDUCATION: Education[] = [
 ]
 
 export const LINKS: Links[] = [
-  { title: "Template website", href: "https://dzmitrykaporski.github.io/website-tamplate/", src: "assets/img/1.avif" },
-  { title: "English for kids", href: "https://dzmitrykaporski-jsfe2021q1-english-for-kids.surge.sh/", src: "assets/img/3.avif" },
-  { title: "Angular movie app", href: "https://final-project_angular13.surge.sh/", src: "assets/img/4.avif" },
-  { title: "Angular CRUD", href: "https://dk-angular-crud.surge.sh/", src: "assets/img/2.avif" },
+  { title: "Template website", href: "https://dzmitrykaporski.github.io/website-tamplate/", src: "assets/img/1.jpg" },
+  { title: "English for kids", href: "https://dzmitrykaporski-jsfe2021q1-english-for-kids.surge.sh/", src: "assets/img/3.jpg" },
+  { title: "Angular movie app", href: "https://final-project_angular13.surge.sh/", src: "assets/img/4.jpg" },
+  { title: "Angular CRUD", href: "https://dk-angular-crud.surge.sh/", src: "assets/img/2.jpg" },
 ]
