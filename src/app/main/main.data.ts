5acfaa1a4bdeb375829ff435383c713496c28e17
Dim@ S <dzmitry.kaporski@gmail.com>
2023-11-13 16:05:06 +0300

    refactor: update link for angular crud

--- src/app/main/main.data.ts
+++ src/app/main/main.data.ts
@@ -4,5 +4,5 @@ export const LINKS: Links[] = [
   { title: "Template website", href: "https://dzmitrykaporski.github.io/website-tamplate/", src: "assets/img/1.jpg" },
   { title: "English for kids", href: "https://dzmitrykaporski-jsfe2021q1-english-for-kids.surge.sh/", src: "assets/img/3.jpg" },
   { title: "Angular movie app", href: "https://final-project_angular13.surge.sh/", src: "assets/img/4.jpg" },
-  { title: "Angular CRUD", href: "https://dk-angular-crud.surge.sh/", src: "assets/img/2.jpg" },
+  { title: "Angular CRUD", href: "https://angular-crud_dk.surge.sh/", src: "assets/img/2.jpg" },
 ]
